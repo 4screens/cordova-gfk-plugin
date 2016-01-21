57922f8adf53fd299f39fad91892781a7b34417b
Bohdan Kostko <bohdan.kostko@nopattern.net>
2016-01-21 17:38:10 +0100

    perf(ios): change log messages + check in sst

--- src/ios/GFKSSTPlugin.m
+++ src/ios/GFKSSTPlugin.m
@@ -26,9 +26,9 @@
     if (adId != nil && [adId length] && mediaId != nil && [mediaId length]) {
         self.SST = [[SST alloc] initWithMediaId:mediaId andAdvertisingId:adId];
 
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"[SSA] Inited with provided Media ID and Ad ID."];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"[SST] Inited with provided Media ID and Ad ID."];
     } else{
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"[SSA] No Media ID :/ How I should send stats..?"];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"[SST] No Media ID :/ How should I send stats..?"];
     }
 
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
@@ -44,7 +44,9 @@
     CDVPluginResult *pluginResult = nil;
     NSString *contentId = [command argumentAtIndex:0 withDefault:@"" andClass:[NSString class]];
 
-    if (contentId != nil && [contentId length]) {
+    if (!self.SST) {
+      pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"[SST] Not inited. Please init first."];
+    } else if (contentId != nil && [contentId length]) {
         [self.SST sendPageImpressionWithContentId:contentId];
 
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[NSString stringWithFormat:@"[SST] Sent impression %@", contentId]];
