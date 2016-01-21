70812e63667c940585901622829297b3e3ade63c
Bohdan Kostko <bohdan.kostko@nopattern.net>
2016-01-21 17:40:43 +0100

    feat(android): android init

---
+++ src/android/GFKSSAPlugin.java
@@ -0,0 +1,12 @@
+package net.nopattern.cordova.gfk;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+
+import android.util.Log;
+
+public class GFKSSAPlugin extends CordovaPlugin {
+
+}
---
+++ src/android/GFKSSTPlugin.java
@@ -0,0 +1,68 @@
+package net.nopattern.cordova.gfk;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+
+import android.util.Log;
+
+import com.gfk.sst.SensicSiteTracker;
+
+public class GFKSSTPlugin extends CordovaPlugin {
+  private SensicSiteTracker tracker;
+  private CordovaWebView appView;
+
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
+    appView = webView;
+  }
+
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if (action.equals("initTraffic")) {
+      String mediaId = args.getString(0);
+      this.initTraffic(mediaId, callbackContext);
+      return true;
+    } else if (action.equals("sendImpression")) {
+      String contentId = args.getString(0);
+      this.sendImpression(contentId, callbackContext);
+      return true;
+    }
+
+    return false;
+  }
+
+  public void initTraffic(String mediaId, CallbackContext callbackContext) {
+    if (mediaId == null){
+      callbackContext.error("[SST] No Media ID :/ How should I send stats..?");
+      return;
+    }
+
+    tracker = new SensicSiteTracker(this.cordova.getActivity().getApplicationContext(), mediaId);
+
+    callbackContext.success("[SST] Inited with provided Media ID and Ad ID.");
+  }
+
+  public void sendImpression(String contentId, CallbackContext callbackContext) {
+    if (contentId == null) {
+      callbackContext.error("[SST] No Content ID :/ How should I send stats..?");
+      return;
+    }
+
+    if (tracker == null) {
+      callbackContext.error("[SST] Not inited. Please init first.");
+      return;
+    }
+
+    tracker.trackPageImpression(contentId);
+    callbackContext.success("[SST] Sent impression");
+  }
+}
