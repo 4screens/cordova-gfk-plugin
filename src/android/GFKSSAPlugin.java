2d14f92f880953d189a9df165905406b6b895d12
Bohdan Kostko <bohdan.kostko@nopattern.net>
2016-01-22 18:43:59 +0100

    feat(android): SSA plugin init and connected

--- src/android/GFKSSAPlugin.java
+++ src/android/GFKSSAPlugin.java
@@ -5,7 +5,15 @@ import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+
 import android.util.Log;
+
+import com.gfk.ssa.SSA;
 
 public class GFKSSAPlugin extends CordovaPlugin {
 
